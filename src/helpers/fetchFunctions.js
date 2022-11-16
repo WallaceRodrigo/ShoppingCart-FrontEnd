40ed646f9d72f8d6371010bf35c19101ff0b8f3d
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2022-11-16 16:40:50 -0300

    2 function fetchProductsList

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,13 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  
+export const fetchProductsList = async (query) => {
+  if (query === undefined) {
+    throw new Error('Termo de busca não informado');
+  }
+
+  const api = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const result = await api.json().then((data) => data.results);
+
+  return result;
 };
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,7 +5,7 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    expect(typeof fetchProductsList()).toBe('function');
+    expect(typeof fetchProductsList).toBe('function');
   });
 
   it('fetch é chamado ao executar fetchProductsList', async () => {
@@ -19,11 +19,11 @@ describe('Teste a função fetchProductsList', () => {
     expect(fetch).toHaveBeenCalledWith(endpoint);
   });
 
-  it('Testa o retorna da função', async() => {
+  it('Testa o retorna da função', async () => {
     expect(await fetchProductsList('computador')).toBe(computadorSearch);
   });
 
-  it('Testa o erro retornado pela função, caso não seja passado nenhum argumento', () => {
-    expect(() => fetchProductsList()).toThrow('Termo de busca não informado');
+  it('Testa o erro retornado pela função, caso não seja passado nenhum argumento', async () => {
+    await expect(fetchProductsList()).rejects.toThrowError('Termo de busca não informado');
   });
 });
