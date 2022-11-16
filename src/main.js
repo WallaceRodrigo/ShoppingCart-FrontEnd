ae0b14aefb265cae36fbced8678a2ca77dbda759
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2022-11-16 19:48:44 -0300

    4 loading text

--- src/main.js
+++ src/main.js
@@ -4,10 +4,32 @@ import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-const productsSection = document.getElementsByClassName('products');
+const productsSection = document.getElementsByClassName('products')[0];
+const loading = document.getElementsByClassName('loading')[0];
 
-const productList = fetchProductsList('computador');
+const createProductList = (query) => {
+  const productList = fetchProductsList(query);
+  loading.innerHTML = 'carregando...';
 
-productList.then((results) => {
-  results.map((product) => productsSection[0].appendChild(createProductElement(product)));
-});
+  productList.then((results) => {
+    results.map((product) => {
+      productsSection
+        .appendChild(createProductElement(product));
+      loading.remove();
+      return '';
+    });
+  });
+
+  return productList;
+};
+
+const catchError = () => {
+  try {
+    createProductList('computador');
+  } catch (error) {
+    loading.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+    console.log(error);
+  }
+};
+
+catchError();
