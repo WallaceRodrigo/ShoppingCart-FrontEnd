abd800ec480224bc7f7cfe18ff1d02135c480483
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2022-11-16 20:05:44 -0300

    6 coverage 100%

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,26 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('fetchProductsList é uma função', () => {
+    expect(typeof fetchProduct).toBe('function');
+  });
+
+  it('fetch é chamado ao executar fetchProduct', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  it('fetch é chamado com o endpoint correto ao executar fetchProducts', async () => {
+    await fetchProduct('MLB1405519561');
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1405519561';
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  it('Testa o retorna da função', async () => {
+    expect(await fetchProduct('MLB1405519561')).toBe(product);
+  });
+
+  it('Testa o erro retornado pela função, caso não seja passado nenhum argumento', async () => {
+    await expect(fetchProduct()).rejects.toThrowError('ID não informado');
   });
 });
